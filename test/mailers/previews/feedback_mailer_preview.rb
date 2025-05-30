8abcb2765dc84bb91bece65fa65471b714f385d3
savexx <johnsaviourlacson@gmail.com>
2025-05-31 01:07:17 +0800

    fix: Update FeedbackMailerPreview to pass the last feedback instance to new_feedback_email method

--- test/mailers/previews/feedback_mailer_preview.rb
+++ test/mailers/previews/feedback_mailer_preview.rb
@@ -1,9 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/feedback_mailer
 class FeedbackMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/feedback_mailer/new_feedback_email
   def new_feedback_email
-    FeedbackMailer.new_feedback_email
+    feedback = Feedback.last
+    FeedbackMailer.new_feedback_email(feedback)
   end
-
 end
