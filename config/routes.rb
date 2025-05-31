795ee00677ba129f8d78e0ab50b63b8ada9c0119
savexx <johnsaviourlacson@gmail.com>
2025-06-01 00:33:27 +0800

    fix: update root route to point to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   resources :posts do
     resources :feedbacks
   end
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
