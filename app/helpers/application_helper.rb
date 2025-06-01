3940c02d9fdbbb2d3fca22e29efda1c94f15c712
savexx <johnsaviourlacson@gmail.com>
2025-06-01 19:05:44 +0800

    feat: Add user avatar component to display user initials in the navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def date_formatter(date)
     date.strftime('%b %d, %Y')
   end
+
+  def user_initials(user)
+    return "" if user.blank?
+
+    first = user.first_name.to_s[0]
+    last = user.last_name.to_s[0]
+
+    "#{first}#{last}".upcase
+  end
 end
