a7b7ee6e49f24c452af12e13fd4c5ab0f1d98252
savexx <johnsaviourlacson@gmail.com>
2025-06-01 23:01:41 +0800

    test: Add RSpec tests for Post model and ensure title and content validations

--- Gemfile
+++ Gemfile
@@ -47,7 +47,6 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
-  gem 'rspec-rails'
 end
 
 group :development do
@@ -74,3 +73,7 @@ gem 'dotenv-rails'
 gem 'bootstrap', '~> 5.3', '>= 5.3.5'
 
 gem 'sassc-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  user = User.find_or_create_by(email: 'johnsaviourlacson@gmail.com') do |u|
+    u.password = 'password'
+    u.password_confirmation = 'password'
+  end
+
+  it 'is has a title' do
+    post = Post.new(title: 'Hello Test', content: 'Testing', user:)
+    expect(post).to be_valid
+  end
+
+  it 'is has a body' do
+    post = Post.new(title: 'Hello Test', content: 'Testing', user:)
+    expect(post).to be_valid
+  end
+end
